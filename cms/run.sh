808b22969c91bf265f7f8a0a78d6395780aedfa9
Margo Sokolova <margo.sockolova2010@yandex.ru>
2024-11-16 10:10:17 +0400

    feat: add develop and prod modes

---
+++ cms/run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo $NODE_ENV
+
+# Check the value of NODE_ENV
+if [ "$NODE_ENV" == "develop" ]; then
+    echo "Running in development mode..."
+    npm run build
+    npm run develop
+elif [ "$NODE_ENV" == "prod" ]; then
+    echo "Running in production mode..."
+    npm run build
+    npm run start
+else
+    echo "NODE_ENV is not set or has an invalid value. Please set it to 'develop' or 'prod'."
+    exit 1
+fi
