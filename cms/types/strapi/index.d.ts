9d9c7eac44cdb7251cf081375a8fdff971fe208c
Margo Sokolova <ritaritarita2013@gmail.com>
2024-12-07 21:53:53 +0400

    fix: delete redundant files and debug configuration

--- cms/types/strapi/index.d.ts
+++
@@ -1,9 +0,0 @@
-import type { Strapi as StrapiType } from '@strapi/strapi';
-import { StrapiMediaService } from '../../src/services/strapi-media.interface';
-
-declare module '@strapi/strapi' {
-  export interface Strapi extends StrapiType {
-    service(uid: 'plugin::strapi-media.strapi-media'): StrapiMediaService;
-    service(uid: string): unknown;
-  }
-}
