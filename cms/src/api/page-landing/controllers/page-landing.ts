4cb1f8415504daf439ea50422ccd682d77bd46e1
meta-panic <ritaritarita2013@gmail.com>
2024-12-24 05:26:51 +0400

    feat: add schemas for landing-page and service-pages && add some reusable components

---
+++ cms/src/api/page-landing/controllers/page-landing.ts
@@ -0,0 +1,7 @@
+/**
+ * page-landing controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::page-landing.page-landing');
---
+++ cms/src/api/page-landing/routes/page-landing.ts
@@ -0,0 +1,7 @@
+/**
+ * page-landing router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::page-landing.page-landing');
