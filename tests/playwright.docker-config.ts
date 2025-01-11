25f377bc13ae3f80d61d5ea5cbafc45be0be7a3e
vysk <v.shlyanchak@gmail.com>
2025-01-11 22:01:04 +0400

    ops: change reported playwright to list

--- tests/playwright.docker-config.ts
+++ tests/playwright.docker-config.ts
@@ -22,7 +22,7 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [ ['html', { open: 'never', outputFolder: './playwright-tests/playwright-report' }] ],
+  reporter: [ ['list', { open: 'never', outputFolder: './playwright-tests/playwright-report' }] ],
   outputDir: './playwright-tests/test-results',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
