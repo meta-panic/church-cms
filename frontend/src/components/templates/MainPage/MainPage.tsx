fa87cfa2423d0d5e4eca0247ea7f9c868b8c0b17
meta-panic <ritaritarita2013@gmail.com>
2025-01-23 01:24:16 +0400

    feat: add 'ábout us' section && do minor refactoring

--- frontend/src/components/templates/MainPage/MainPage.tsx
+++ frontend/src/components/templates/MainPage/MainPage.tsx
@@ -1,61 +1,60 @@
 "use client";
-import { StaticImageData } from "next/image";
 import cx from "classnames";
 
 import { Hero } from "@/components/organisms/hero/Hero";
+import { AboutUs } from "@/components/organisms/aboutUs/AboutUs";
 import Section from "@/components/atoms/section/Section";
 import Typography from "@/components/atoms/typography/Typography";
-import { ComponentContentBlocksInfoBlock as HeroBlock } from "@/types";
+import { ComponentContentBlocksInfoBlock } from "@/types";
 import Button from "@/components/atoms/Button/Button";
 import { BREAKPOINTS, useMediaQuery } from "@/hooks/useMediaQuery";
 import ClientOnly from "@/components/organisms/header/_components/ClientOnly";
+import srcBackgroundHeroImage from "/public/background.jpg";
 
 import styles from "./MainPage.module.css";
 
 
 interface MainPageProps {
-  srcBackgroundHeroImage: StaticImageData;
-  heroData: HeroBlock;
+  heroData: ComponentContentBlocksInfoBlock;
+  aboutUs: ComponentContentBlocksInfoBlock;
 }
 
 export const MainPage: React.FC<MainPageProps> = ({
-  srcBackgroundHeroImage,
-  heroData,
+  heroData, aboutUs,
 }) => {
   const isMobile = useMediaQuery(BREAKPOINTS.mobile);
 
   return (
     <>
-      {
-        <Hero
-          src={srcBackgroundHeroImage}
-          content={
-            <div className={cx("darkBlock", styles.heroContainer)}>
-              <div className={styles.hero}>
-                <div className={styles.text}><Typography tag="H1">{heroData.Title}</Typography></div>
-                <div>
-                  {heroData.description?.map((description) => {
-                    return <div className={styles.text} key={description?.id}><Typography tag="body">{description?.body}</Typography><br /></div>;
-                  })}
-                </div>
-                <ClientOnly>
-                  <Button
-                    link="/internal-page"
-                    variant="ghost"
-                    text={heroData.Button?.Button_text || "Узнать больше"}
-                    on="onBrand"
-                    size="L"
-                    wide={!!isMobile}
-                  />
-                </ClientOnly>
+      <Hero
+        src={srcBackgroundHeroImage}
+        content={
+          <div className={cx("darkBlock", styles.heroContainer)}>
+            <div className={styles.hero}>
+              <div className={styles.text}><Typography tag="H1">{heroData.Title}</Typography></div>
+              <div>
+                {heroData.description?.map((description) => {
+                  return <div className={styles.text} key={description?.id}><Typography tag="body">{description?.body}</Typography><br /></div>;
+                })}
               </div>
+              <ClientOnly>
+                <Button
+                  link="/internal-page"
+                  variant="ghost"
+                  text={heroData.Button?.Button_text || "Узнать больше"}
+                  on="onBrand"
+                  size="L"
+                  wide={!!isMobile}
+                />
+              </ClientOnly>
             </div>
-          }
-        />
-      }
+          </div>
+        }
+      />
+
 
       <Section>
-        <div style={{ backgroundColor: "red" }}>section-1</div>
+        <AboutUs aboutUs={aboutUs} />
       </Section>
 
       <Section className={styles.section}>
